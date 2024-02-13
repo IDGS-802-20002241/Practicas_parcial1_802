c36a56a38f1529f652b9a4a1bfcbc1ed15edc371
IDGS-802-20002241 <156141194+IDGS-802-20002241@users.noreply.github.com>
2024-02-12 20:00:07 -0600

    Examen Resistencia OMG 12/02/2024
    Examen Resistencia OMG 12/02/2024

--- forms.py
+++ forms.py
@@ -3,9 +3,8 @@ from wtforms import StringField, TextAreaField,SelectField,RadioField
 from wtforms import EmailField
 
 class number(Form):
-    num1=StringField("Punto 1")
-    num2=StringField("Punto 2")
-    num3=StringField("Punto 3")
-    num4=StringField("Punto 4")
-
+    num1=SelectField("Banda 1",choices=[('', 'SELECCIONA'),('', 'NEGRO'), ('1', 'CAFÉ'), ('2', 'ROJO'), ('3', 'NARANJA'), ('4', 'AMARILLO'), ('5', 'VERDE'), ('6', 'AZUL'), ('7', 'VIOLETA'), ('8', 'GRIS'), ('9', 'BLANCO')])
+    num2=SelectField("Banda 2",choices=[('', 'SELECCIONA'),('', 'NEGRO'), ('1', 'CAFÉ'), ('2', 'ROJO'), ('3', 'NARANJA'), ('4', 'AMARILLO'), ('5', 'VERDE'), ('6', 'AZUL'), ('7', 'VIOLETA'), ('8', 'GRIS'), ('9', 'BLANCO')])
+    num3=SelectField("Banda 3",choices=[('', 'SELECCIONA'),('', 'NEGRO'), ('0', 'CAFÉ'), ('00', 'ROJO'), ('000', 'NARANJA'), ('0000', 'AMARILLO'), ('00000', 'VERDE'), ('000000', 'AZUL'), ('0000000', 'VIOLETA'), ('00000000', 'GRIS'), ('000000000', 'BLANCO')])
+    num4=RadioField('Banda 4 (Tolerancia)',choices=[('.05','Dorada:5%'),('.1','Plateada:10%')])
    
--- main.py
+++ main.py
@@ -33,13 +33,14 @@ def calculo():
 def distancia():
     resultado = None
     number_form=forms.number(request.form)
+    operacion = request.form.get("operacion")
     if request.method == "POST":
         
         num1 = int(number_form.num1.data)
         num2 = int(number_form.num2.data)
       
         num3 = int(number_form.num3.data)
-        num4 = int(number_form.num4.data)
+        num4 = request.form.get("operacion")
       
         resultado= math.sqrt((num2 - num1)**2 + (num4 - num3)**2)
        
@@ -47,6 +48,60 @@ def distancia():
            
 
     return render_template("distancia.html", resultado=resultado, form = number_form)
+
+@app.route("/resistencias", methods=["GET", "POST"])
+def resistencia():
+    resultado = ''
+    resultado2 = 0.0 
+    resultado3 = 0.0 
+    num1 = 0  # Valor inicial
+    num2 = 0  # Valor inicial
+    num3 = 0  # Valor inicial
+    num4 = 0.0  # Valor inicial
+
+    number_form = forms.number(request.form)
+    
+    if request.method == "POST":
+        num1 = number_form.num1.data
+        num2 = number_form.num2.data
+        num3 = number_form.num3.data
+        num4 = float(number_form.num4.data)
+      
+        resultado = str(num1) + str(num2)+str(num3)
+        resultado2 = float(resultado) - (float(resultado) * num4)
+        resultado3 = float(resultado) + (float(resultado) * num4)
+
+    return render_template("resistencia.html", resultado= resultado, resultado2=resultado2, resultado3=resultado3,
+                           form=number_form, color=obtener_color(str(num1)), color2=obtener_color(str(num2)),
+                           color3=obtener_color(str(num3)), color4=obtener_color(num4))
+
+def obtener_color(num):
+    if num == '':
+        return 'black'
+    elif num == .05:
+        return 'gold'
+    elif num == .1:
+        return 'silver'
+    elif num == '1' or num == '0':
+        return 'brown'
+    elif num == '2' or num == '00':
+        return 'red'
+    elif num == '3' or num == '000':
+        return 'orange'
+    elif num == '4' or num == '0000':
+        return 'yellow'
+    elif num == '5' or num == '00000':
+        return 'green'
+    elif num == '6' or num == '000000':
+        return 'blue'
+    elif num == '7' or num == '0000000':
+        return 'violet'
+    elif num == '8' or num == '00000000':
+        return 'gray'
+    else:
+        return 'white'
+
+
    
 if __name__ == "__main__":
     app.run(debug=True)   
